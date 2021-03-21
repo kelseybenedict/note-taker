71a5b8dafbb115ccb149b27bda009096da221ceb
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-03-21 17:31:35 -0600

    added file structure, commented on server.js

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+// requiring express to be able to access it
+const express = require('express');
+
+// creating an express server
+const app = express();
+
+// setting initial port
+const PORT = process.env.PORT || 3000;
+
+// set up for data parsing -- express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routing to apiRoutes & htmlRoutes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+// creating the listener and notification to check if it's working
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
